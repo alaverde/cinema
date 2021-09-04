fecb7c96c3c6ec9e44816a85ce866ccd19f45c63
Melissamonsalve <melissa_monsalve82171@elpoli.edu.co>
2021-09-04 18:07:40 -0500

    terminando las pruebas

--- service-users/src/test/java/com/politecnico/serviceuser/UserServiceMockTest.java
+++ service-users/src/test/java/com/politecnico/serviceuser/UserServiceMockTest.java
@@ -1,7 +1,15 @@
 package com.politecnico.serviceuser;
 
+import com.politecnico.serviceuser.repositories.UserRepository;
+import com.politecnico.serviceuser.services.UserService;
+import org.junit.jupiter.api.BeforeEach;
+import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class UserServiceMockTest {
+
+    @Mock
+    private UserRepository userRepository;
+    private UserService userService;
 }
